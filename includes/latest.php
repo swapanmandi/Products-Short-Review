0de53d9e5afdbffdc3115ad16bfb3ea84fe27097
swapanmandi <96920652+swapanmandi@users.noreply.github.com>
2023-01-07 18:26:34 +0530

    Add files via upload

---
+++ includes/latest.php
@@ -0,0 +1,20 @@
+<?php 
+include'header.php';
+?>
+
+<div class="about">
+<p>
+<h4>GearPatrol.com</h4>
+This is an interesting style review website that reads more like a magazine than a review site. 
+This definitely helps give it some more credit than throwing a up a bunch of products and 
+hoping people read them. It’s interesting that it takes a different approach, rather than 
+writing long content, it focuses on a clean layout and video reviews to show people 
+the product they’re testing, racking in 2.3 million monthly viewers. This is incredibly v
+aluable for people willing to buy, but want to see the product in a video demo before purchasing. 
+Now compare this site to what the site looked like in 2008 (here).
+</p>
+</div>
+
+<?php
+include'footer.php';
+?>
