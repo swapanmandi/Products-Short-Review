5475db4d108d96989994c66ba1de98a79e99013c
swapanmandi <96920652+swapanmandi@users.noreply.github.com>
2023-01-13 21:54:38 +0530

    Create cart.php

---
+++ includes/cart.php
@@ -0,0 +1,29 @@
+?php 
+include 'conn.php';
+session_start();
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  
+  
+  
+  </head>
+<body>
+
+<?php
+  
+  
+  
+  <?php
+include 'footer.php';
+?>
+
+</body>
+</html>
