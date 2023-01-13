b6f836f6c6cd3eb679e2c0e48e8f40e579b65dfc
swapanmandi <96920652+swapanmandi@users.noreply.github.com>
2023-01-13 21:51:41 +0530

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,56 +1,20 @@
 
 <?php
-include 'header.php';
-session_start();
+error_reporting(0);
+
+
+
+$servername = "localhost";
+$username ="root";
+$password ="";
+$database ="pro_lan_db";
+
+$con = mysqli_connect($servername, $username, $password, $database);
+
+if(!$con){
+	die("error".mysqli_connect_error());
+}
+else{	//echo " database connect successfully!!"."<br>"; 
+}
+
 ?>
-<!--from databas-->
-
-	  <?php
-
-include 'conn.php';
-
-//for cart section
-
-if (isset($_POST['add_to_cart'])) {
-
-if (isset($_SESSION['cart'])){
-   $item_array_id = array_column($_SESSION['cart'], 'item_id');
-   if (!in_array($_GET['id'], $item_array_id)) {
-       $count = count($_SESSION['cart']);
-       $_SESSION['count'] = $count;
-       $item_array = array(
-        'item_id' => $_GET['id'],
-        'item_name' => $_POST['name'],
-        'item_image' => $_POST['image'],
-        'item_link' => $_POST['link']
-    );
-    $_SESSION['cart'] [$count] = $item_array;
-    echo "<script>window.location.</script>";
-    //echo 'product is added';
-    header('location:cart.php');
-   }
-   else{
-    //echo "<script>window.location='test1.php'</script>";
-      // echo "product is already added to cart!";
-       //echo "<script>window.location='test1.php'</script>";
-        //echo "<script>window.location.search.php</script>";
-        header('location:cart.php');
-   }
-
-}
-else{
-
-    $item_array = array(
-        'item_id' => $_GET['id'],
-        'item_name' => $_POST['name'],
-        'item_image' => $_POST['image'],
-        'item_link' => $_POST['link']
-    );
-    $_SESSION['cart'] [0] = $item_array;
-    
-
-}
-    
-}
-
-//session_destroy();
