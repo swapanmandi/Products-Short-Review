ea6582bd97756e0d2d9eb0941b84111255618785
swapanmandi <96920652+swapanmandi@users.noreply.github.com>
2023-01-15 19:19:44 +0530

    Create search-page.php

---
+++ includes/search-page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+  
+  
+  <link rel="icon" href="upload/icon.png" type="image/icon">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>TheShortReviews- Helps you to buy best Products, Gadgets,Gifts and more.</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Cache-control" content="no-cache">
+  
+  
+  <meta name="keywords" content="products, gadegts, cool gadgets, amazing products, gift">
+  <meta name="description" content="The Short Reviews helps You to find all categories of amazing products, cool gadgets and unique gift ideas">
+  <meta name="robots" content="index, follow">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+	
+  </head>
+<body>
