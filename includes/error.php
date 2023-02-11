06e4623d0503bd652f639f96da23468381a8c2bc
swapanmandi <96920652+swapanmandi@users.noreply.github.com>
2023-02-11 19:03:48 +0530

    Add files via upload

---
+++ includes/error.php
@@ -0,0 +1,11 @@
+<?php
+include'header.php';
+?>
+<div class="err-p">
+Sorry! The page you are looking for could not be found.<br><br>
+Can go back to <a href="">the homepage.</a>
+
+</div>
+<?php
+include'footer.php';
+?>
