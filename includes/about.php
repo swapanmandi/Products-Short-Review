8968f44b4217e5eaa772a7ec1a8502c2904b0240
swapanmandi <96920652+swapanmandi@users.noreply.github.com>
2023-02-11 19:05:18 +0530

    Add files via upload

---
+++ includes/about.php
@@ -0,0 +1,18 @@
+<?php 
+include'header.php';
+?>
+
+
+<div class="page about">
+
+<p>
+<h2>About Us</h2>
+
+The Short Reviews will help you to find best products.So, you eaily buy a amazing, cool,interresting , innovatve products listed on this website. We highlight the features of the  products that helps you to get a products which wants. If you will  buy a product from the referral from this site, we get a small comission. We do not carry  any products and we dont sell anything listed on this site.
+</p>
+
+</div>
+
+<?php
+include'footer.php';
+?>
